d3484bc820178495711855984832da1fb1061bdf
Alexey Shevtsov <alexey.shevtsov@wrenkitchens.com>
2020-08-25 20:20:13 +0300

    Trying to configure mysql

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,6 +22,22 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'number' => $number,
+        ]);
+    }
+
+    /**
+     * @Route("/product", name="product")
+     *
+     * @ param Product $product
+     *
+     * @return Response
+     */
+    public function getProduct(/*Product $product*/): Response
+    {
+phpinfo(); exit;
+
+        return $this->render('home/product.html.twig', [
+//            'number' => $number,
         ]);
     }
 }
